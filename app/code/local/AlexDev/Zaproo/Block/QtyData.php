59991df7c216dc290d0be0c90bde62992c382488
Alex Reznichenko <alx.reznichenko72@gmail.com>
2016-11-29 17:56:58 +0200

    firs Block and work with QTY

---
+++ app/code/local/AlexDev/Zaproo/Block/QtyData.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dev30
+ * Date: 11/29/16
+ * Time: 1:42 PM
+ */
+class AlexDev_Zaproo_Block_QtyData extends Mage_Core_Block_Template
+{
+    public function getProductsQtyData() {
+
+        $product_collection = Mage::getModel('catalog/product')
+            ->getCollection()
+            ->addAttributeToSelect('*')
+            ->joinField('qty',
+                'cataloginventory/stock_item',
+                'qty',
+                'product_id=entity_id',
+                '{{table}}.stock_id=1',
+                'left'
+            )->addAttributeToSort('name', 'ASC');;
+
+       return $product_collection;
+    }
+}
